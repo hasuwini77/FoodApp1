abc5f5f9d213e1a0e92770de3f3c1a342bdc4fa2
Edwin Andermyr <andermyredwin@gmail.com>
2024-05-20 17:17:31 +0200

    initialised repo, created two component and whole fetching logic inside tsx file

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,10 @@
+const Header = () => { 
+
+    return ( 
+        <header > 
+            <h1 className="text-3xl w-full text-center m-0"> Welcome to Da App</h1>
+        </header>
+    )
+}
+
+export default Header; 
---
+++ src/app/components/MainContent/index.tsx
@@ -0,0 +1,59 @@
+'use client'
+import React, { useEffect, useState } from "react";
+
+// Define the type for the meal data
+interface Meal {
+    idMeal: string;
+    strMeal: string;
+    strMealThumb: string;
+    strInstructions: string;
+}
+
+interface MealResponse {
+    meals: Meal[];
+}
+
+const MainContent: React.FC = () => {
+    const [meal, setMeal] = useState<Meal | null>(null);
+
+    useEffect(() => {
+        if (meal) {
+            console.log(meal);
+        }
+    }, [meal]);
+
+    const handleClick = async () => {
+        setMeal(null); // Reset meal to ensure fetch happens on button click
+        const fetchData = async () => {
+            try {
+                const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+                const data: MealResponse = await response.json();
+                setMeal(data.meals[0]);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        await fetchData();
+    };
+
+    return (
+        <>
+            <div className="flex justify-center align-center p-10 mt-12 text-2xl">
+                <button onClick={handleClick}>Get Random Meal</button>
+            </div>
+
+            <div>
+                {meal && (
+                    <div>
+                        <h2>{meal.strMeal}</h2>
+                        <img src={meal.strMealThumb} alt={meal.strMeal} />
+                        <p className="text-center p-2 mt-4 mb-4 leading-loose tracking-wide w-[250px] md:w-full">{meal.strInstructions}</p>
+                    </div>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default MainContent;
